a854784a6128e9b654350ffe4faffd0ff57f648e
Subin <72715023+lightsaber29@users.noreply.github.com>
2021-03-12 11:12:47 +0900

    secondcommit

---
+++ src/kosta/mvc/controller/HelloController.java
@@ -0,0 +1,8 @@
+package kosta.mvc.controller;
+
+public class HelloController {
+	public static void main(String[] args) {
+		System.out.println("HelloController");
+		
+	}
+}
--- src/kosta/mvc/controller/TestController.java
+++ src/kosta/mvc/controller/TestController.java
@@ -3,5 +3,7 @@ package kosta.mvc.controller;
 public class TestController {
 	public void insert() {
 		System.out.println("insert 메소드입니다.");
+		
+		System.out.println("변경해봤습니다.");
 	}
 }
